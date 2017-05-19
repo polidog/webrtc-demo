b73c2d3956dd1396528277c6c42e9a9d9a3ea274
polidog <polidogs@gmail.com>
2017-05-19 14:24:02 +0900

    ejs化

--- index.js
+++ index.js
@@ -2,12 +2,27 @@ const app = require('express')();
 const http = require('http').Server(app);
 const PORT = process.env.PORT || 8080;
 const IO_PORT = process.env.IO_PORT || 9001
+var ejs = require('ejs');
 
 const io = require('socket.io').listen(IO_PORT);
 
+app.engine('ejs', ejs.renderFile);
 
 app.get(`/sdp`, (req, res) => {
-  res.sendFile(__dirname + '/sdp/index.html');
+    console.log();
+    var domain = req.headers.host.split(':')[0];
+    var io_script_path = "http://" + domain + ':' + IO_PORT + '/socket.io/socket.io.js';
+    var io_path = 'http://' + domain + ':' + IO_PORT;
+    res.render('sdp.ejs',
+        {
+            title : 'Express + EJS' ,
+            content: '大分シンプルになった！',
+            io_script_path: io_script_path,
+            io_path: io_path
+        }
+    );
+
+    // res.sendFile(__dirname + '/sdp/index.html');
 });
 
 app.get(`/ice`, (req, res) => {
